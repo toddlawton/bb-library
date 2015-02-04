e0fc4d8a0d590d91e8eba1da4aecbf49be7e64fe
Todd Beauchamp <toddbee@gmail.com>
2015-02-04 12:36:41 -0500

    Added main REST API functions

--- server.js
+++ server.js
@@ -27,10 +27,16 @@ app.get( '/api', function( request, response ) {
 mongoose.connect( 'mongodb://localhost/library_database' );
 
 // Schemas
+
+var Keywords = new mongoose.Schema({
+	keyword: String
+});
+
 var Book = new mongoose.Schema({
 	title: String,
 	author: String,
-	releaseDate: Date
+	releaseDate: Date,
+	keywords: [Keywords]
 });
 
 // Models
@@ -62,4 +68,65 @@ app.get( '/api/books', function( request, response ) {
 			return console.log( err );
 		}
 	});
+});
+
+app.get( '/api/books/:id', function ( request, response) {
+	return BookModel.findById( request.params.id, function( err, book ) {
+		if ( !err ) {
+			return response.send( book );
+		} else {
+			return console.log( err );
+		}
+	});
+});
+
+app.post( '/api/books', function( request, response ) {
+	var book = new BookModel({
+		title: request.body.title,
+		author: request.body.author,
+		releaseDate: request.body.releaseDate,
+		keywords: request.body.keywords
+	});
+
+	return book.save( function( err ){
+		if ( !err ) {
+			console.log( 'created' );
+			return response.send( book );
+		} else {
+			console.log( err );
+		}
+	} );
+});
+
+app.put( '/api/books/:id', function( request, response ) {
+	console.log('Updating book '+ request.body.title);
+	return BookModel.findById( request.params.id, function( err, book ) {
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate,
+		book.keywords = request.body.keywords
+
+		return book.save( function( err ) {
+			if (!err) {
+				console.log( 'book updated' );
+				return response.send( book );
+			} else {
+				console.log( err );
+			}
+		});
+	});
+});
+
+app.delete( '/api/books/:id', function( request, response ) {
+	console.log( 'Deleting book with id: ' + request.params.id);
+	return BookModel.findById( request.params.id, function(err, book) {
+		return book.remove(function(err){
+			if (!err) {
+				console.log('Book removed.');	
+				return response.send('');
+			} else {
+				console.log(err);
+			}
+		});
+	}); 
 });
