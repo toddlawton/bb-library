341960987bb51fc2a4f75e274df8ed122b64b45d
Todd Beauchamp <toddbee@gmail.com>
2015-02-03 19:01:15 -0500

    Add and delete functions

--- js/views/book.view.js
+++ js/views/book.view.js
@@ -8,8 +8,17 @@ app.BookView = Backbone.View.extend({
 
     bookTemplate: _.template($('#book-template').html()),
 
+    events: {
+    	'click .delete': 'deleteBook'
+    },
+
     render: function() {
         this.$el.html( this.bookTemplate( this.model.attributes ) );
         return this;
+    },
+
+    deleteBook: function() {
+    	this.model.destroy(); // Destroy the model
+    	this.remove(); // Destroy the view
     }
 });
--- js/views/library.view.js
+++ js/views/library.view.js
@@ -2,11 +2,18 @@ var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
 
-    el: '.book-list',
+    el: '.container',
+
+    events: {
+        'click #add': 'addBook'
+    },
 
     initialize: function(initialBooks) {
+        this.$booksList = this.$el.find('.book-list');
         this.collection = new app.Library( initialBooks );
         this.render();
+
+        this.listenTo(this.collection, 'add', this.render);
     },
 
     render: function() {
@@ -19,7 +26,21 @@ app.LibraryView = Backbone.View.extend({
         var bookView = new app.BookView({
             model: item
         });
-        this.$el.append(bookView.render().el);
+        this.$booksList.append(bookView.render().el);
+    },
+
+    addBook: function(e) {
+        e.preventDefault();
+
+        var newBookAttributes = {};
+
+        this.$('input').each(function(index, el){
+            if ($(el).val() !== '') {
+                newBookAttributes[el.id] = $(el).val();    
+            }
+        });
+
+        this.collection.add( new app.Book(newBookAttributes));
     }
 
 });
